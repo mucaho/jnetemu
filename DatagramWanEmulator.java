016db1772fad0a02b7230922646c110974b13a79
mucaho <mkucko@gmail.com>
2016-04-08 18:39:00 +0100

    Properly shutdown emulation threads

--- DatagramWanEmulator.java
+++ DatagramWanEmulator.java
@@ -1,6 +1,6 @@
-/**
+/*
  * The MIT License (MIT)
- * Copyright (c) 2013 mucaho (http://mucaho.ddns.net/)
+ * Copyright (c) 2013-2016 mucaho (https://github.com/mucaho)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy 
  * of this software and associated documentation files (the "Software"), to deal 
@@ -22,272 +22,297 @@
  */
 
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A convenience class for debugging UDP communication. The DatagramWanEmulator sits between
- * two specified {@link DatagramSocket}s and introduces latency, jitter and package loss 
+ * two specified {@link DatagramSocket}s and introduces package latency, jitter, loss and duplication
  * between these two sockets.
  * <p>
  * In order to use the emulator, you send the {@link DatagramPacket}s directly 
  * to the emulator's {@link java.net.SocketAddress} instead of sending them to the receiver 
  * {@link java.net.SocketAddress}. 
- * The emulator will introduce latency, jitter and package loss. Then it will send 
- * the packets to the receiving socket.
+ * The emulator will introduce package latency, jitter, loss and duplication.
+ * Then it will send the packets to the receiving socket.
  * <p>
- * This class can be instantiated with the respective addresses. Use the appropriate methods
+ * This class can be instantiated using the respective addresses. Use the appropriate methods
  * to start and stop the emulation.
- * The specific emulation parameters can be tuned with the appropriate getters / setters. 
+ * The specific emulation parameters can be tuned with the appropriate getters / setters,
+ * before starting the emulation or while emulation is already active.
  * 
  * @author mucaho
  *
  */
 public class DatagramWanEmulator {
-	/**
-	 * Internally used. Queues up delayed sending of datagram packets
-	 */
-	private static final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+    /**
+     * Internally used. Number of active emulators.
+     */
+    private final static AtomicInteger emulatorCount = new AtomicInteger(0);
 
-	/** The socket address A. */
-	private final InetSocketAddress socketAddressA;
-	
-	/** The socket address B. */
-	private InetSocketAddress socketAddressB;
-	
-	/** The emulator socket. */
-	private DatagramSocket emulatorSocket;
+    /**
+     * Internally used. Queues up delayed sending of datagram packets
+     */
+    private static ScheduledThreadPoolExecutor executor;
 
-	/**
-	 * Construct a new DatagramWanEmulator. This will automatically create a new socket 
-	 * bound to the specified emulatorAddress. The socketAddressB will be captured the
-	 * first time a packet is sent from socketAddressB.
-	 * 
-	 * @param emulatorAddress	the emulator address
-	 * @param socketAddressA	one socket address
-	 * @throws SocketException	the socket exception
-	 */
-	public DatagramWanEmulator(InetSocketAddress emulatorAddress, InetSocketAddress socketAddressA) 
-		throws SocketException {
-		this(emulatorAddress, socketAddressA, null);
-	}
 
-	/**
-	 * Construct a new DatagramWanEmulator. This will automatically create a new socket 
-	 * bound to the specified emulatorAddress.
-	 *
-	 * @param emulatorAddress	the emulator address
-	 * @param socketAddressA	one socket address
-	 * @param socketAddressB	the other socket address
-	 * @throws SocketException	the socket exception
-	 */
-	public DatagramWanEmulator(InetSocketAddress emulatorAddress, InetSocketAddress socketAddressA, 
-			InetSocketAddress socketAddressB) throws SocketException {
-		this.socketAddressA = socketAddressA;
-		this.socketAddressB = socketAddressB;
-		
-		emulatorSocket = new DatagramSocket(emulatorAddress);
-	}
-	
-	/**
-	 * The maximum size of incoming / outcoming datagram packets (in bytes).
-	 * Defaults to 1024.
-	 */
-	private int maxPacketLength = 1024;
-	/**
-	 * The amount of package loss. A value of 0.0f means no package loss, 
-	 * a value of 1.0f means every packet will be lost. 
-	 * Defaults to 0.1f (10%).
-	 */
-	private volatile float packageLoss = 0.1f;
+    /** The socket address A. */
+    private final SocketAddress socketAddressA;
 
-	/**
-	 * The chance of a packet being duplicated. A value of 0.0f means no
-	 * packets will be duplicated, a value of 1.0f means every packet will
-	 * be duplicated infinitely.
-	 * Defaults to 0.03f (3%).
-	 */
-	private volatile float packageDuplication = 0.03f;
-	/**
-	 * The maximum latency between sending from one host to receiving on the other one.
-	 * The latency will vary between minLatency and maxLatency.
-	 * Defaults to 250 ms.
-	 */
-	private volatile int maxLatency = 250;
-	
-	/**
-	 * The minimum latency between sending from one host to receiving on the other one.
-	 * The latency will vary between minLatency and maxLatency.
-	 * Defaults to 100 ms.
-	 */
-	private volatile int minLatency = 100;
-	
-	/**
-	 * Gets the maximum size of incoming / outcoming datagram packets (in bytes).
-	 * Defaults to 1024.
-	 *
-	 * @return the max packet length
-	 */
-	public int getMaxPacketLength() {
-		return maxPacketLength;
-	}
+    /** The socket address B. */
+    private SocketAddress socketAddressB;
 
-	/**
-	 * Sets the maximum size of incoming / outcoming datagram packets (in bytes).
-	 * Defaults to 1024.
-	 *
-	 * @param maxPacketLength the new max packet length
-	 */
-	public void setMaxPacketLength(int maxPacketLength) {
-		this.maxPacketLength = maxPacketLength;
-	}
+    /** The emulator socket. */
+    private DatagramSocket emulatorSocket;
 
-	/**
-	 * Gets the amount of package loss. 
-	 * A value of 0.0f means no package loss, a value of 1.0f means every packet will be lost.
-	 * Defaults to 0.1f (== 10%).
-	 *
-	 * @return the package loss percentage
-	 */
-	public float getPackageLoss() {
-		return packageLoss;
-	}
+    /**
+     * Construct a new DatagramWanEmulator. This will automatically create a new socket
+     * bound to the specified emulatorAddress. The socketAddressB will be captured the
+     * first time a packet is sent from socketAddressB to socketAddressA through this emulator.
+     *
+     * @param emulatorAddress	the emulator address
+     * @param socketAddressA	one socket address
+     * @throws SocketException	the socket exception
+     */
+    public DatagramWanEmulator(SocketAddress emulatorAddress, SocketAddress socketAddressA)
+        throws SocketException {
+        this(emulatorAddress, socketAddressA, null);
+    }
 
-	/**
-	 * Sets the amount of package loss. 
-	 * A value of 0.0f means no package loss, a value of 1.0f means every packet will be lost.
-	 * Defaults to 0.1f (== 10%).
-	 *
-	 * @param percentage the new package loss percentage
-	 */
-	public void setPackageLoss(float percentage) {
-		this.packageLoss = percentage;
-	}
-	
-	
-	/**
-	 * Gets the chance of a packet being duplicated. 
-	 * A value of 0.0f means no packets will be duplicated, 
-	 * a value of 1.0f means every packet will be duplicated infinitely.
-	 * Defaults to 0.03f (3%).
-	 */
-	public float getPackageDuplication() {
-		return packageDuplication;
-	}
+    /**
+     * Construct a new DatagramWanEmulator. This will automatically create a new socket
+     * bound to the specified emulatorAddress.
+     *
+     * @param emulatorAddress	the emulator address
+     * @param socketAddressA	one socket address
+     * @param socketAddressB	the other socket address
+     * @throws SocketException	the socket exception
+     */
+    public DatagramWanEmulator(SocketAddress emulatorAddress, SocketAddress socketAddressA,
+							   SocketAddress socketAddressB) throws SocketException {
+        this.socketAddressA = socketAddressA;
+        this.socketAddressB = socketAddressB;
 
-	/**
-	 * Sets the chance of a packet being duplicated. 
-	 * A value of 0.0f means no packets will be duplicated, 
-	 * a value of 1.0f means every packet will be duplicated infinitely.
-	 * Defaults to 0.03f (3%).
-	 */
-	public void setPackageDuplication(float packageDuplication) {
-		this.packageDuplication = packageDuplication;
-	}
+        emulatorSocket = new DatagramSocket(emulatorAddress);
+    }
 
-	/**
-	 * Gets the maximum latency between sending from one host to receiving on the other one.
-	 * The latency will vary between minLatency and maxLatency. Defaults to 250 ms.
-	 *
-	 * @return the max latency
-	 */
-	public int getMaxLatency() {
-		return maxLatency;
-	}
+    /**
+     * The maximum size of incoming / outcoming datagram packets (in bytes).
+     * Defaults to 1024.
+     */
+    private int maxPacketLength = 1024;
+    /**
+     * The amount of package loss. A value of 0.0f means no package loss,
+     * a value of 1.0f means every packet will be lost.
+     * Defaults to 0.1f (10%).
+     */
+    private volatile float packageLoss = 0.1f;
 
-	/**
-	 * Sets the maximum latency between sending from one host to receiving on the other one.
-	 * The latency will vary between minLatency and maxLatency. Defaults to 250 ms.
-	 *
-	 * @param maxLatency the new max latency
-	 */
-	public void setMaxLatency(int maxLatency) {
-		this.maxLatency = maxLatency;
-	}
+    /**
+     * The chance of a packet being duplicated. A value of 0.0f means no
+     * packets will be duplicated, a value of 1.0f means every packet will
+     * be duplicated infinitely.
+     * Defaults to 0.03f (3%).
+     */
+    private volatile float packageDuplication = 0.03f;
+    /**
+     * The maximum latency between sending from one host to receiving on the other one.
+     * The latency will vary between minLatency and maxLatency.
+     * Defaults to 250 ms.
+     */
+    private volatile int maxLatency = 250;
 
-	
-	/**
-	 * Gets the minimum latency between sending from one host to receiving on the other one.
-	 * The latency will vary between minLatency and maxLatency. Defaults to 100 ms.
-	 *
-	 * @return the min latency
-	 */
-	public int getMinLatency() {
-		return minLatency;
-	}
+    /**
+     * The minimum latency between sending from one host to receiving on the other one.
+     * The latency will vary between minLatency and maxLatency.
+     * Defaults to 100 ms.
+     */
+    private volatile int minLatency = 100;
 
-	/**
-	 * Sets the minimum latency between sending from one host to receiving on the other one.
-	 * The latency will vary between minLatency and maxLatency. Defaults to 100 ms.
-	 *
-	 * @param minLatency the new min latency
-	 */
-	public void setMinLatency(int minLatency) {
-		this.minLatency = minLatency;
-	}
-	
-	/** The runnable. */
-	private final Runnable runnable = new Runnable() {
-		
-		@Override
-		public void run() { try {
-			
-			while(true) {
-				byte[] bytes = new byte[maxPacketLength];
-				final DatagramPacket packet = new DatagramPacket(bytes, maxPacketLength);
-				emulatorSocket.receive(packet);
-				
-				if ((socketAddressB == null) && 
-						!(socketAddressA.equals(packet.getSocketAddress())))
-					socketAddressB = (InetSocketAddress) packet.getSocketAddress();
-				
-				if (socketAddressA.equals(packet.getSocketAddress()))
-					packet.setSocketAddress(socketAddressB);
-				else if (socketAddressB.equals(packet.getSocketAddress()))
-					packet.setSocketAddress(socketAddressA);
-				
-				do {
-					
-					if (Math.random() >= packageLoss)
-						executor.schedule(new Runnable() {
-							
-							@Override public void run() { try {
-								if (!emulatorSocket.isClosed())
-									emulatorSocket.send(packet);
-							} catch(Exception e) {e.printStackTrace();}}
-							
-						}, minLatency + (int)(Math.random()* (maxLatency - minLatency)), 
-						TimeUnit.MILLISECONDS);
-					
-				} while (Math.random() < packageDuplication);
-			}
-				
-				
-		} catch (SocketException se) {/*closed*/} catch (Exception e) { e.printStackTrace();}}
-	};
-	
-	/**
-	 * Start the emulation between the two sockets.
-	 * @throws IllegalStateException if the current instance has already been stopped by stopEmulation(). 
-	 * Create new 
-	 * {@link DatagramWanEmulator#DatagramWanEmulator(InetSocketAddress, InetSocketAddress, InetSocketAddress) instance} 
-	 * instead.
-	 */
-	public void startEmulation() throws IllegalStateException {
-		if (emulatorSocket.isClosed()) 
-			throw new IllegalStateException("This instance has already been stopped. Create a new one.");
-		
-		new Thread(runnable).start();
-	}
-	
-	/**
-	 * Let the emulation end and close the associated emulator socket.
-	 */
-	public void stopEmulation() {
-		emulatorSocket.close();
-	}
+    /**
+     * Gets the maximum size of incoming / outcoming datagram packets (in bytes).
+     * Defaults to 1024.
+     *
+     * @return the max packet length
+     */
+    public int getMaxPacketLength() {
+        return maxPacketLength;
+    }
+
+    /**
+     * Sets the maximum size of incoming / outcoming datagram packets (in bytes).
+     * Defaults to 1024.
+     *
+     * @param maxPacketLength the new max packet length
+     */
+    public void setMaxPacketLength(int maxPacketLength) {
+        this.maxPacketLength = maxPacketLength;
+    }
+
+    /**
+     * Gets the amount of package loss.
+     * A value of 0.0f means no package loss, a value of 1.0f means every packet will be lost.
+     * Defaults to 0.1f (== 10%).
+     *
+     * @return the package loss percentage
+     */
+    public float getPackageLoss() {
+        return packageLoss;
+    }
+
+    /**
+     * Sets the amount of package loss.
+     * A value of 0.0f means no package loss, a value of 1.0f means every packet will be lost.
+     * Defaults to 0.1f (== 10%).
+     *
+     * @param percentage the new package loss percentage
+     */
+    public void setPackageLoss(float percentage) {
+        this.packageLoss = percentage;
+    }
+
+
+    /**
+     * Gets the chance of a packet being duplicated.
+     * A value of 0.0f means no packets will be duplicated,
+     * a value of 1.0f means every packet will be duplicated infinitely.
+     * Defaults to 0.03f (3%).
+     */
+    public float getPackageDuplication() {
+        return packageDuplication;
+    }
+
+    /**
+     * Sets the chance of a packet being duplicated.
+     * A value of 0.0f means no packets will be duplicated,
+     * a value of 1.0f means every packet will be duplicated infinitely.
+     * Defaults to 0.03f (3%).
+     */
+    public void setPackageDuplication(float packageDuplication) {
+        this.packageDuplication = packageDuplication;
+    }
+
+    /**
+     * Gets the maximum latency between sending from one host to receiving on the other one.
+     * The latency will vary between minLatency and maxLatency. Defaults to 250 ms.
+     *
+     * @return the max latency
+     */
+    public int getMaxLatency() {
+        return maxLatency;
+    }
+
+    /**
+     * Sets the maximum latency between sending from one host to receiving on the other one.
+     * The latency will vary between minLatency and maxLatency. Defaults to 250 ms.
+     *
+     * @param maxLatency the new max latency
+     */
+    public void setMaxLatency(int maxLatency) {
+        this.maxLatency = maxLatency;
+    }
+
+
+    /**
+     * Gets the minimum latency between sending from one host to receiving on the other one.
+     * The latency will vary between minLatency and maxLatency. Defaults to 100 ms.
+     *
+     * @return the min latency
+     */
+    public int getMinLatency() {
+        return minLatency;
+    }
+
+    /**
+     * Sets the minimum latency between sending from one host to receiving on the other one.
+     * The latency will vary between minLatency and maxLatency. Defaults to 100 ms.
+     *
+     * @param minLatency the new min latency
+     */
+    public void setMinLatency(int minLatency) {
+        this.minLatency = minLatency;
+    }
+
+    /** The thread. */
+    private final Thread thread = new Thread(new Runnable() {
+
+        @Override
+        public void run() { try {
+
+            while(!Thread.currentThread().isInterrupted()) {
+                byte[] bytes = new byte[maxPacketLength];
+                final DatagramPacket packet = new DatagramPacket(bytes, maxPacketLength);
+                emulatorSocket.receive(packet);
+
+                if ((socketAddressB == null) &&
+                        !(socketAddressA.equals(packet.getSocketAddress())))
+                    socketAddressB = packet.getSocketAddress();
+
+                if (socketAddressA.equals(packet.getSocketAddress()))
+                    packet.setSocketAddress(socketAddressB);
+                else if (socketAddressB.equals(packet.getSocketAddress()))
+                    packet.setSocketAddress(socketAddressA);
+
+                do {
+
+                    if (Math.random() >= packageLoss)
+                        executor.schedule(new Runnable() {
+
+                            @Override public void run() { try {
+                                if (!emulatorSocket.isClosed())
+                                    emulatorSocket.send(packet);
+                            } catch(Exception e) {e.printStackTrace();}}
+
+                        }, minLatency + (int)(Math.random()* (maxLatency - minLatency)),
+                        TimeUnit.MILLISECONDS);
+
+                } while (Math.random() < packageDuplication);
+            }
+
+
+        } catch (SocketException se) { /*closed*/ } catch (Exception e) { e.printStackTrace(); }}
+    });
+
+    /**
+     * Start the emulation between the two sockets.
+     * @throws IllegalStateException if the current instance has already been stopped by stopEmulation().
+     * Create new
+     * {@link DatagramWanEmulator#DatagramWanEmulator(SocketAddress, SocketAddress, SocketAddress) instance}
+     * instead.
+     */
+    public void startEmulation() throws IllegalStateException {
+        if (emulatorSocket.isClosed())
+            throw new IllegalStateException("This instance has already been stopped. Create a new one.");
+
+        // setup up a new STPE if this is the first emulator instance
+        synchronized (DatagramWanEmulator.emulatorCount) {
+            if (emulatorCount.getAndIncrement() == 0) {
+                executor = new ScheduledThreadPoolExecutor(2);
+            }
+        }
+
+        // start this instance
+        thread.start();
+    }
+
+    /**
+     * Let the emulation end and close the associated emulator socket.
+     * @throws InterruptedException if the current instance is interrupted while stopping emulation
+     */
+    public void stopEmulation() throws InterruptedException {
+        // stop this instance
+        thread.interrupt();
+        emulatorSocket.close();
+		thread.join();
+
+        // destroy the old STPE if this is was the last emulator instance
+        synchronized (DatagramWanEmulator.emulatorCount) {
+            if (emulatorCount.decrementAndGet() == 0) {
+                executor.shutdown();
+                executor = null;
+            }
+        }
+    }
 }
--- TestWanEmulator.java
+++ TestWanEmulator.java
@@ -1,70 +1,77 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
 
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.nio.ByteBuffer;
 
 public class TestWanEmulator {
 
-	private final static int BYTE_COUNT = Integer.SIZE / Byte.SIZE;
+    private final static int BYTE_COUNT = Integer.SIZE / Byte.SIZE;
 
-	/**
-	 * @param args
-	 * @throws SocketException 
-	 */
-	public static void main(String[] args) throws Exception {
-		
-		// setup the addresses (the ports are randomly chosen)
-		InetSocketAddress serverAddress = new InetSocketAddress(InetAddress.getLocalHost(), 26010);
-		InetSocketAddress clientAddress = new InetSocketAddress(InetAddress.getLocalHost(), 26011);
-		InetSocketAddress emulatorAddress = new InetSocketAddress(InetAddress.getLocalHost(), 26012);
-		
-		// setup the sockets
-		final DatagramSocket serverSocket = new DatagramSocket(serverAddress);
-		DatagramSocket clientSocket = new DatagramSocket(clientAddress);
-		// setup emulator socket
-		DatagramWanEmulator emu = 
-			new DatagramWanEmulator(emulatorAddress, serverAddress);
-	
-		
-		
-		// start the wan emulation
-		emu.startEmulation();
-		
-		
-		// receive the packets at server
-		final ByteBuffer serverBuffer = ByteBuffer.allocate(BYTE_COUNT);
-		final DatagramPacket serverPacket = 
-			new DatagramPacket(serverBuffer.array(), BYTE_COUNT);
-		new Thread(new Runnable() {
-			@Override
-			public void run() { try {
-				while(true) {
-					serverSocket.receive(serverPacket);
-					int i = serverBuffer.getInt();
-					System.out.printf("[Server]: recving #%02d \n", i);
-					serverBuffer.clear();
-				}
-			} catch (IOException e) { e.printStackTrace(); }}
-		}).start(); // start the newly created thread
-		
-		
-		// send the packets from the client to the wan emulator
-		ByteBuffer clientBuffer = ByteBuffer.allocate(BYTE_COUNT);
-		DatagramPacket clientPacket = 
-			new DatagramPacket(clientBuffer.array(), BYTE_COUNT, emulatorAddress);
-		for (int i=0; i<100; i++) {
-			System.out.printf("[Client]: sending #%02d \n", i);
-			clientBuffer.clear(); 
-			clientBuffer.putInt(i);
-			clientPacket.setData(clientBuffer.array());
-			clientSocket.send(clientPacket);
-			Thread.sleep(10); // just a bit of delay
-		}
-	}
+
+    public static void main(String[] args) throws Exception {
+        // setup the addresses (the ports are randomly chosen)
+        InetSocketAddress serverAddress = new InetSocketAddress(InetAddress.getLocalHost(), 26010);
+        InetSocketAddress clientAddress = new InetSocketAddress(InetAddress.getLocalHost(), 26011);
+        InetSocketAddress emulatorAddress = new InetSocketAddress(InetAddress.getLocalHost(), 26012);
+
+        // setup the sockets
+        final DatagramSocket serverSocket = new DatagramSocket(serverAddress);
+        DatagramSocket clientSocket = new DatagramSocket(clientAddress);
+        // setup emulator socket
+        DatagramWanEmulator emu =
+            new DatagramWanEmulator(emulatorAddress, serverAddress);
+
+
+
+        // start the wan emulation
+        emu.startEmulation();
+
+
+        // receive the packets at server
+        final ByteBuffer serverBuffer = ByteBuffer.allocate(BYTE_COUNT);
+        final DatagramPacket serverPacket =
+            new DatagramPacket(serverBuffer.array(), BYTE_COUNT);
+        final Thread serverThread = new Thread(new Runnable() {
+            @Override
+            public void run() { try {
+
+                while(!Thread.currentThread().isInterrupted()) {
+                    serverSocket.receive(serverPacket);
+                    int i = serverBuffer.getInt();
+                    System.out.printf("[Server]: recving #%02d \n", i);
+                    serverBuffer.clear();
+                }
+
+            } catch (SocketException e) { /*closed */ } catch (Exception e) { e.printStackTrace(); }}
+        });
+        serverThread.start(); // start the newly created thread
+
+
+        // send the packets from the client to the wan emulator
+        ByteBuffer clientBuffer = ByteBuffer.allocate(BYTE_COUNT);
+        DatagramPacket clientPacket =
+            new DatagramPacket(clientBuffer.array(), BYTE_COUNT, emulatorAddress);
+        for (int i=0; i<100; i++) {
+            System.out.printf("[Client]: sending #%02d \n", i);
+            clientBuffer.clear();
+            clientBuffer.putInt(i);
+            clientPacket.setData(clientBuffer.array());
+            clientSocket.send(clientPacket);
+            Thread.sleep(10); // just a bit of delay
+        }
+
+
+        // shutdown: kill all threads by interrupting and exceptionally closing their sockets
+        Thread.sleep(1000);
+        serverThread.interrupt();
+        clientSocket.close();
+        emu.stopEmulation();
+        serverSocket.close();
+        serverThread.join();
+
+    }
 
 }
